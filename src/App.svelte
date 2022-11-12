07d594c8f058e454f6ee03a93efecb69fe5f03c6
userNu11ified <108763797+userNu11ified@users.noreply.github.com>
2022-11-12 21:02:45 +0100

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,7 +2,27 @@
 	// A new project!
 </script>
 
-<div class="app-mount" />
+<div class="app-mount">
+
+</div>
 
 <style>
+	:global(body) {
+    	position: absolute;
+		
+		width: 100vw;
+		height: 100vh;
+
+		margin: 0;
+		padding: 0;
+		border: 0;
+		outline: 0;
+	}
+	
+	.app-mount {
+		width: 100%;
+		height: 100%;
+
+		background-color: rgb(30, 30, 30);
+	}
 </style>
